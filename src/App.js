b0449c9307bdcfd4554ac454f742dad623448faa
therdean <dejanristevski96@gmail.com>
2023-12-05 12:38:44 +0100

    test v2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div style={{ backgroundColor: 'black', width: '100vw', height: '100vh', color: 'white', fontSize: '30px' }} className="App">
-      TEST JENKINS
+      TEST JENKINSSsss
     </div>
   );
 }
